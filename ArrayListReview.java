a0fec826e8d4afaf908b3b95cdc2d6da8b01471b
Laura Williams <107360787+laura-williams-1@users.noreply.github.com>
2025-08-29 14:34:41 -0400

    inital commit

---
+++ ArrayListReview.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Main(
+
+
+    public static void main(String[] args){
+
+        ArrayList<String> names = new ArrayList <>();
+
+        names.add("Laura");
+        names.add("Chris");
+
+        name.toString();
+    }
+)
